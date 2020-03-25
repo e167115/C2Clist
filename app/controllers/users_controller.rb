3ca3f78bba3899c596b028af5cf870247fa0183c
yuta <e167115ty@gmail.com>
2020-03-25 10:59:57 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,40 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "会員登録を完了しました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "会員登録に失敗しました"
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "会員情報を更新しました"
+      redirect_to @user
+    else
+      flash[:danger] = "会員情報を更新できませんでした"
+      render :edit
+    end
   end
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile)
 end
