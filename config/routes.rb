e0bf192939e56849a8993ade4716f01275d17ca3
yuta <e167115ty@gmail.com>
2020-03-26 11:21:44 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
     root to: "toppages#index"
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create, :edit, :update,]
+    
+    get "login", to: "sessions#new"
+    post "login", to:"sessions#create"
+    delete "logout", to:"sessions#destroy"
+    
 end
 
